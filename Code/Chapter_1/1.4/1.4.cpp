0d39d82e5240cd1c123567855f3e7168de4a01fd
Jack <908988818@qq.com>
2018-12-12 14:54:24 +0800

    1.4

---
+++ Code/Chapter_1/1.4/1.4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout <<"please cin 2 number;"<<endl;
+	int a = 0;
+	int b = 0;
+	cin >> a >> b;
+	cout <<"the result is "<<a*b<<endl;
+	return 0;
+}
