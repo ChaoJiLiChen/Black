a0a24f59badae16dc52355ca09d12ef522211c62
Jack <908988818@qq.com>
2019-04-11 15:49:32 +0800

    C++开发实战第三章

---
+++ C++开发实战/第三章/婚礼上的谎言/婚礼上的谎言/婚礼上的谎言/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  婚礼上的谎言
+//
+//  Created by Chen on 2019/4/11.
+//  Copyright © 2019年 Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
