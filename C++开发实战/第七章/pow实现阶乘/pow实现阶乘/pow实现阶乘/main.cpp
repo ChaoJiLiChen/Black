d86573c18d3a0ea23cc9db22cc6e2e671c467712
Jack <908988818@qq.com>
2019-05-23 16:52:06 +0800

    pow阶乘

---
+++ C++开发实战/第七章/pow实现阶乘/pow实现阶乘/pow实现阶乘/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  pow实现阶乘
+//
+//  Created by Chen on 2019/5/23.
+//  Copyright © 2019 Chen. All rights reserved.
+//
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    int x;
+    int y;
+    cin>>x;
+    cin>>y;
+    cout <<pow(x,y)<<endl;
+    
+    return 0;
+}
